6e725f4fdfae6558876496ab6f222b5a5583245a
Britte Breindl <britte.breindl@gmail.com>
2019-11-20 12:35:07 +0100

    ananonymize config

---
+++ config.js
@@ -0,0 +1,9 @@
+//
+export default config = {
+  apiKey: '<YOUR GOOGLE API KEY>',
+  dbUrl: '<YOUR DATABASE URL>',
+  loginUrl:
+    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=<YOUR GOOGLE API KEY>',
+  signupUrl:
+    'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=<YOUR GOOGLE API KEY>'
+};
